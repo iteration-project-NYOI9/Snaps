69839f6d925c8440680e995a03f4d51d05a6100c
thomas444ortiz <thomasortiz444@gmail.com>
2024-01-09 20:08:16 -0500

    changed to hash router

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import LoginContainer from "./LoginContainer.jsx";
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import './stylesheets/styles.css';
 import SnapsContainer from "./SnapsContainer.jsx";
 
